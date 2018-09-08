29dc565fbec89a37b06601b1c030bf130ddca917
masontang@aol.com <masontang@aol.com>
2018-09-08 10:51:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector(){
+  $('p')
+}
+
+function lastImageSelector(){
+  $('img:last:child')
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector(){
+  $('.pics')
+}
+
+function firstListItem(){
+  $('#pic-list ul li img:first:child')
+}
+
